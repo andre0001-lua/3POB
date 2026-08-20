63ab97e52c6eee4c3e1590807dfe8d77c529b53f
andre0001-lua <andre.25204708360001@faeterj-rio.edu.br>
2026-08-20 04:47:39 -0300

    Commitando a resolução do exercício 03

---
+++ Exercicio03.java
@@ -0,0 +1,25 @@
+//calculo de salario usando valor hora trabalho com horas trabalhadas e desconto do inss de 10% no final mostrar salario bruto e liquido
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        System.out.print("Digite o valor da hora de trabalho: ");
+        double valorHora = scanner.nextDouble();
+
+        System.out.print("Digite o número de horas trabalhadas: ");
+        double horasTrabalhadas = scanner.nextDouble();
+
+        double salarioBruto = valorHora * horasTrabalhadas;
+        double descontoINSS = salarioBruto * 0.10;
+        double salarioLiquido = salarioBruto - descontoINSS;
+
+        System.out.println("Salário Bruto: R$ " + df.format(salarioBruto));
+        System.out.println("Salário Líquido: R$ " + df.format(salarioLiquido));
+
+        scanner.close();
+    }
+}
