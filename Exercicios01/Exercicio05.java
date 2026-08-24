aff38c564563ff26c1c2675b137e124d39c66ebf
andore <alpfp2007@gmail.com>
2026-08-24 11:00:21 -0300

    Exercicio 5 incompleto

---
+++ Exercicios01/Exercicio05.java
@@ -0,0 +1,16 @@
+package _3POB.Exercicios01;
+
+import java.util.Scanner;
+ 
+public class Exercicio05 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite os segundos: ");
+        int segundos = scan.nextInt();
+
+
+        minutos = segundos / 60;
+        horas = minutos / 60;
+    }
+}
